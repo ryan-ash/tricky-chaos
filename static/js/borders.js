755858bf5e798efde8dc3b6ecdf7395e9bba6fb1
Ryan Ash <scarletspark@gmail.com>
2023-03-27 03:14:11 +0200

    added initial dashed lines

--- static/js/borders.js
+++ static/js/borders.js
@@ -9,91 +9,127 @@ let prevHorizontalIndex = null;
 let prevVerticalIndex = null;
 
 function createLineParts() {
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
 
-  const numHorizontalLines = Math.ceil(windowWidth / lineSize);
-  const numVerticalLines = Math.ceil(windowHeight / lineSize);
+    const numHorizontalLines = Math.ceil(windowWidth / lineSize);
+    const numVerticalLines = Math.ceil(windowHeight / lineSize);
 
-  $('#left, #right').addClass('vertical-container');
+    $('#left, #right').addClass('vertical-container');
 
-  for (let i = 0; i < numHorizontalLines; i++) {
-    const horizontalLine = $(`<div class="line horizontal" data-x="${i}"></div>`);
-    $('#top').append(horizontalLine);
-    horizontalLinePartsTop[i] = horizontalLine;
-    const horizontalLineBottom = horizontalLine.clone();
-    $('#bottom').append(horizontalLineBottom);
-    horizontalLinePartsBottom[i] = horizontalLineBottom;
-  }
+    for (let i = 0; i < numHorizontalLines; i++) {
+        const horizontalLine = $(`<div class="line horizontal" data-x="${i}"></div>`);
+        $('#top').append(horizontalLine);
+        horizontalLinePartsTop[i] = horizontalLine;
+        const horizontalLineBottom = horizontalLine.clone();
+        $('#bottom').append(horizontalLineBottom);
+        horizontalLinePartsBottom[i] = horizontalLineBottom;
+    }
 
-  for (let i = 0; i < numVerticalLines; i++) {
-    const verticalLine = $(`<div class="line vertical" data-y="${i}"></div>`);
-    $('#left').append(verticalLine);
-    verticalLinePartsLeft[i] = verticalLine;
-    const verticalLineRight = verticalLine.clone();
-    $('#right').append(verticalLineRight);
-    verticalLinePartsRight[i] = verticalLineRight;
-  }
+    for (let i = 0; i < numVerticalLines; i++) {
+        const verticalLine = $(`<div class="line vertical" data-y="${i}"></div>`);
+        $('#left').append(verticalLine);
+        verticalLinePartsLeft[i] = verticalLine;
+        const verticalLineRight = verticalLine.clone();
+        $('#right').append(verticalLineRight);
+        verticalLinePartsRight[i] = verticalLineRight;
+    }
 }
 
-function markItem(list, startingIndex) {
-  list[startingIndex].addClass('current-cursor');
 
-  adjacentClasses = ['current-cursor-adjacent', 'current-cursor-adjacent-two', 'current-cursor-adjacent-three'];
+function createAdjacentStyles(numAdjacentParts) {
+    const styles = [];
+    const minOpacity = 0.5;
 
-  for (let i = 0; i < adjacentClasses.length; i++) {
-    actualOffset = i + 1;
-    lowerIndex = startingIndex - actualOffset;
-    higherIndex = startingIndex + actualOffset;
+    for (let i = 0; i < numAdjacentParts; i++) {
+        const rotation = 90 - 90 * (i + 1) / (numAdjacentParts + 1);
+        const opacity = minOpacity + ((1 - minOpacity) * (i + 1) / (numAdjacentParts + 1));
+        const backgroundColor = Math.floor(85 + (170 * (numAdjacentParts - i)) / (numAdjacentParts + 1));
 
-    if (list[lowerIndex]) {
-      list[lowerIndex].addClass(adjacentClasses[i]);
+        styles.push({
+            transform: `rotate(${rotation}deg)`,
+            opacity: opacity,
+            backgroundColor: `rgb(${backgroundColor}, ${backgroundColor}, ${backgroundColor})`,
+        });
     }
-    if (list[higherIndex]) {
-      list[higherIndex].addClass(adjacentClasses[i]);
-    }
-  }
+
+    return styles;
 }
 
-function updateCursorLines(x, y) {
-  const horizontalIndex = Math.floor(x / lineSize);
-  const verticalIndex = Math.floor(y / lineSize);
 
-  if (horizontalIndex !== prevHorizontalIndex || verticalIndex !== prevVerticalIndex) {
-    classesToClear = ['current-cursor', 'current-cursor-adjacent', 'current-cursor-adjacent-two', 'current-cursor-adjacent-three'];
-    for (let i = 0; i < classesToClear.length; i++) {
-      $(`.${classesToClear[i]}`).removeClass(classesToClear[i]);
-    }
+function markItem(list, startingIndex, adjacentStyles) {
+    list[startingIndex].addClass('current-cursor');
 
-    if (horizontalLinePartsTop[horizontalIndex]) {
-      markItem(horizontalLinePartsTop, horizontalIndex);
-    }
-    if (horizontalLinePartsBottom[horizontalIndex]) {
-      markItem(horizontalLinePartsBottom, horizontalIndex);
-    }
-    if (verticalLinePartsLeft[verticalIndex]) {
-      markItem(verticalLinePartsLeft, verticalIndex);
-    }
-    if (verticalLinePartsRight[verticalIndex]) {
-      markItem(verticalLinePartsRight, verticalIndex);
-    }
+    for (let i = 0; i < adjacentStyles.length; i++) {
+        const lowerIndex = startingIndex - (i + 1);
+        const higherIndex = startingIndex + (i + 1);
 
-    prevHorizontalIndex = horizontalIndex;
-    prevVerticalIndex = verticalIndex;
-  }
+        if (list[lowerIndex]) {
+            list[lowerIndex].css(adjacentStyles[i]);
+        }
+        if (list[higherIndex]) {
+            list[higherIndex].css(adjacentStyles[i]);
+        }
+    }
 }
+
+
+function updateCursorLines(x, y, adjacentStyles) {
+    const horizontalIndex = Math.floor(x / lineSize);
+    const verticalIndex = Math.floor(y / lineSize);
+
+    if (horizontalIndex !== prevHorizontalIndex || verticalIndex !== prevVerticalIndex) {
+        $(".current-cursor").each(function() {
+            $(this).removeClass("current-cursor");
+            const dataIndex = $(this).hasClass("horizontal") ? "x" : "y";
+
+            for (let i = 0; i < adjacentStyles.length; i++) {
+                const lowerIndex = parseInt($(this).data(dataIndex)) - (i + 1);
+                const higherIndex = parseInt($(this).data(dataIndex)) + (i + 1);
+
+                const lines = $(this).parent().children();
+
+                if (lines[lowerIndex]) {
+                    $(lines[lowerIndex]).removeAttr("style");
+                }
+                if (lines[higherIndex]) {
+                    $(lines[higherIndex]).removeAttr("style");
+                }
+            }
+        });
+
+        if (horizontalLinePartsTop[horizontalIndex]) {
+            markItem(horizontalLinePartsTop, horizontalIndex, adjacentStyles);
+        }
+        if (horizontalLinePartsBottom[horizontalIndex]) {
+            markItem(horizontalLinePartsBottom, horizontalIndex, adjacentStyles);
+        }
+        if (verticalLinePartsLeft[verticalIndex]) {
+            markItem(verticalLinePartsLeft, verticalIndex, adjacentStyles);
+        }
+        if (verticalLinePartsRight[verticalIndex]) {
+            markItem(verticalLinePartsRight, verticalIndex, adjacentStyles);
+        }
+
+        prevHorizontalIndex = horizontalIndex;
+        prevVerticalIndex = verticalIndex;
+    }
+}
+
 
 $(document).ready(function () {
-  createLineParts();
-
-  $(window).resize(function () {
-    $('#top, #bottom, #left, #right').empty();
     createLineParts();
-  });
+    const numAdjacentParts = 5;
+    const adjacentStyles = createAdjacentStyles(numAdjacentParts);
 
-  $(document).mousemove(function (e) {
-    const x = e.clientX;
-    const y = e.clientY;
-    updateCursorLines(x, y);
-  });
+    $(window).resize(function () {
+        $('#top, #bottom, #left, #right').empty();
+        createLineParts();
+    });
+
+    $(document).mousemove(function (e) {
+        const x = e.clientX;
+        const y = e.clientY;
+        updateCursorLines(x, y, adjacentStyles);
+    });
 });
--- static/js/cursor-lines.js
+++ static/js/cursor-lines.js
@@ -17,6 +17,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.body.appendChild(cursorLines);
 
+    let dashedLines = [];
+
+    function createDashedLines() {
+        for (let i = 0; i < 4; i++) {
+            const dashedLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+            dashedLine.style.stroke = "#555";
+            dashedLine.style.opacity = 0.22;
+            dashedLine.style.strokeWidth = "1px";
+            dashedLine.style.strokeDasharray = "22, 50";
+            dashedLine.style.strokeDashoffset = "20";
+            cursorLines.appendChild(dashedLine);
+            dashedLines.push(dashedLine);
+        }
+    }
+
+    function updateDashedLines(x, y) {
+        const borderPositions = [
+            { x: prevHorizontalIndex * lineSize + 10, y: 0 },
+            { x: prevHorizontalIndex * lineSize + 10, y: window.innerHeight },
+            { x: 0, y: prevVerticalIndex * lineSize + 3 },
+            { x: window.innerWidth, y: prevVerticalIndex * lineSize + 3 },
+        ];        
+
+        for (let i = 0; i < dashedLines.length; i++) {
+            dashedLines[i].setAttribute("x1", borderPositions[i].x);
+            dashedLines[i].setAttribute("y1", borderPositions[i].y);
+            dashedLines[i].setAttribute("x2", x);
+            dashedLines[i].setAttribute("y2", y);
+        }
+    }
+
+    function animateDashedLines() {
+        for (let i = 0; i < dashedLines.length; i++) {
+            const currentOffset = parseFloat(dashedLines[i].style.strokeDashoffset);
+            dashedLines[i].style.strokeDashoffset = currentOffset - 1;
+        }
+        requestAnimationFrame(animateDashedLines);
+    }
+
     function distance(x1, y1, x2, y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
@@ -53,7 +92,12 @@ document.addEventListener("DOMContentLoaded", function () {
             lines[i].setAttribute("y2", y);
             lines[i].style.stroke = `rgba(0, 100, 100, ${opacity})`;
         }
+
+        updateDashedLines(x, y);
     });
+
+    createDashedLines();
+    animateDashedLines();
     
     // Hide the cursor
     document.body.style.cursor = "none";
