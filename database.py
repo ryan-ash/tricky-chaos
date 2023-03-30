895fa68f61b49ac20da45c08bdb2b1bf20d046d5
Ryan Ash <scarletspark@gmail.com>
2023-03-30 16:55:33 +0200

    added links to projects (be) + flaskenv

--- database.py
+++ database.py
@@ -7,3 +7,10 @@ class Project(db.Model):
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     image = db.Column(db.String(200), nullable=False)
+    links = db.relationship('Link', backref='project', lazy=True)
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
