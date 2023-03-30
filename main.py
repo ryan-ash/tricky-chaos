500db3fcca8baa51cea41e2588d09518f8cbd6ea
Ryan Ash <scarletspark@gmail.com>
2023-03-30 18:17:59 +0200

    initial admin panel

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, make_response
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+from flask_httpauth import HTTPBasicAuth
 from flask_migrate import Migrate
 from config import config
-from database import db, Project
+from database import db, Project, Link
 
 app = Flask(__name__)
 app.config.from_mapping(config)
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if username == app.config['ADMIN_USERNAME'] and password == app.config['ADMIN_PASSWORD']:
+        return True
+    return False
+
+class AuthenticatedModelView(ModelView):
+    def is_accessible(self):
+        if not auth.current_user():
+            return False
+        auth_result = auth.login_required(lambda: True)()
+        if isinstance(auth_result, Response):
+            return False
+        return True
+
+    def inaccessible_callback(self, name, **kwargs):
+        return auth.login_required(lambda: None)()
+
+class AuthenticatedAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        if not auth.current_user():
+            return False
+        auth_result = auth.login_required(lambda: True)()
+        if isinstance(auth_result, Response):
+            return False
+        return True
+
+    def inaccessible_callback(self, name, **kwargs):
+        return auth.login_required(lambda: None)()
+
+admin = Admin(app, name='Chaotic Sandbox', template_mode='bootstrap3', index_view=AuthenticatedAdminIndexView())
+admin.add_view(AuthenticatedModelView(Project, db.session))
+admin.add_view(AuthenticatedModelView(Link, db.session))
 
 @app.route('/')
 def home():
